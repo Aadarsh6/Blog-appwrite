bc9a276b5dec926b4640a17447fbf75b765dadc9
Aadarsh Mishra <Aadarsh6@users.noreply.github.com>
2025-05-23 11:25:32 +0530

    login, Signup component

--- src/Component/index.js
+++ src/Component/index.js
@@ -2,9 +2,14 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 export { 
         Header,
         Footer,
         Container,
-        LogoutBtn
+        LogoutBtn,
+        Button,
+        Input,
+        
      }
